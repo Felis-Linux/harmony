fb7fabd8c7dd1c4e19b0a5f7c932952ab0ee2193
Ohio2 <sekmartyna4@gmail.com>
2024-06-03 19:04:50 +0200

    update hmnstrcut and smprintf to not be bugged todo: packagestrGet functions (invalid regex)

--- src/harmony/strings.c
+++ src/harmony/strings.c
@@ -7,35 +7,37 @@
 #include "strings.h"
 #include "hmndefines.h"
 
-HMN_UNUSED_MALLOC char *hmnstrcut(char *str, size_t from, size_t to) {
-  size_t cut_strlen = from - to;
-  str += from;
-  char *ret = nullptr;
-
-  if(str[0] == '\0') 
-      return ret;
-
-  if((! (from < to)) && to > strlen(str)) {
-    return nullptr;
-  }
-  
-  ret = malloc(cut_strlen*sizeof(char));
-  strncpy(ret, str, cut_strlen);
-  return ret;
-}
-
 [[gnu::format(printf, 1, 2)]] HMN_UNUSED_MALLOC char *smprintf(const char *format, ...) {
   va_list args;
   va_start(args, format);
-  
-  size_t len = vsnprintf(NULL, 0, format, args);
+  size_t len = vsnprintf(nullptr, 0, format, args);
   char *buf = malloc(sizeof(char)*(len + 1));
   if(buf == nullptr)
-      return nullptr;
+      goto err_or_ok;
+  
+  va_end(args);
+  
+  va_start(args, format);
 
   vsnprintf(buf, len + 1, format, args);
+
+err_or_ok:
   va_end(args);
+  
   return buf;
+}
+
+HMN_UNUSED_MALLOC char *hmnstrcut(char *str, size_t from, size_t to) {
+  if(str == nullptr || from >= to || to > strlen(str))
+      return nullptr;
+
+  size_t cut_strlen = to - from;
+  str += from;
+  char *ret = malloc((cut_strlen + 1)*sizeof(char));
+  strncpy(ret, str, cut_strlen);
+  ret[cut_strlen] = '\0';
+
+  return ret;
 }
 
 StringsError_t pkgstringGetVersion(const char *pkgstring) {
--- tests/arenat.c
+++ tests/arenat.c
@@ -6,16 +6,39 @@
 void setUp() {}
 void tearDown() {}
 
-void arenatest() {
-  Arena_t arena = {0};
-  char *mystringpointer = arenaAlloc(&arena, sizeof(char)*3);
+void arenatest1_allocanddestroy() {
+  Arena_t *arena = arenaInit();
+  char *mystringpointer = arenaAlloc(arena, sizeof(char)*3);
   mystringpointer = "hi";
-  UNITY_TEST_FAIL(strcmp("hi", mystringpointer), "oh no");
-  arenaDestroy(&arena);
+  TEST_ASSERT_TRUE(!strcmp(mystringpointer, "hi"));
+  arenaDestroy(arena);
+}
+
+void arenatest2_realloc() {
+  Arena_t *arena = arenaInit();
+  int *myarray = arenaAlloc(arena, sizeof(int)*8);
+  int *myarraypre;
+  myarray[0] = 0;
+  myarray[1] = 1;
+  myarraypre = myarray;
+  myarray = arenaRealloc(arena, myarray, sizeof(int)*8, sizeof(int)*16);
+  TEST_ASSERT_EQUAL_INT_ARRAY(myarraypre, myarray, 2);
+  arenaDestroy(arena);
+}
+
+void arenatest3_astrdup() {
+  Arena_t *arena = arenaInit();
+  char mystring[3] = "hi";
+  char *dollysstring = astrdup(arena, mystring);
+  TEST_ASSERT_TRUE(!strcmp(mystring, dollysstring));
+  arenaDestroy(arena);
 }
 
 int main() {
   UNITY_BEGIN();
-  RUN_TEST(arenatest);
+  RUN_TEST(arenatest1_allocanddestroy);
+  RUN_TEST(arenatest2_realloc);
+  RUN_TEST(arenatest3_astrdup);
+  UNITY_END();
 }
 
